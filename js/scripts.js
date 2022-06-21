d7deaf085e6d52caadd1496e7263886cbdc7166c
Alex <saa01011@yandex.ru>
2022-06-21 17:27:21 +0300

    saad studio

---
+++ js/scripts.js
@@ -0,0 +1,170 @@
+let slideFlag = {};
+let slidePointer = {};
+let galFlag = false;
+const russMonth = ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'];
+
+function retimer() {
+    let limit = new Date($('.retaimer').data('fordate'));
+    let now = new Date();
+    let delta = Math.floor((limit.getTime() - now.getTime()) / 1000);
+    if (delta < 0) delta = 0;
+    let sec = delta % 60;
+    $('.retaimer .num').eq(3).html(`${addChar(sec)}<span class="subnum">${multiple(sec, ['секунда', 'секунды', 'секунд'])}</span>`);
+    delta = Math.floor(delta / 60);
+    let minute = delta % 60;
+    $('.retaimer .num').eq(2).html(`${addChar(minute)}<span class="subnum">${multiple(minute, ['минута', 'минуты', 'минут'])}</span>`);
+    delta = Math.floor(delta / 60);
+    let hour = delta % 24;
+    $('.retaimer .num').eq(1).html(`${addChar(hour)}<span class="subnum">${multiple(hour, ['час', 'часа', 'часов'])}</span>`);
+    delta = Math.floor(delta / 24);
+    $('.retaimer .num').eq(0).html(`${delta}<span class="subnum">${multiple(delta, ['день', 'дня', 'дней'])}</span>`);
+}
+
+function addChar(c) {
+    c += '';
+    if (c.length < 2) {
+        c = '0' + c;
+    }
+    return c;
+}
+
+function multiple(num, words) {
+    num = num % 100;
+    if (Math.floor(num / 10) != 1) {
+        if (num % 10 == 1) {
+            return words[0];
+        } else if ((num % 10 > 1) && (num % 10 < 5)) {
+            return words[1];
+        }
+    }
+    return words[2];
+}
+
+function sliderRun(slideclass, direction) {
+    if (slideFlag[slideclass]) return;
+    slideFlag[slideclass] = true;
+    let selector = '.' + slideclass + '_block';
+    let width = $(selector + '.active').width();
+    let next, anim;
+    if (direction == 'toright') {
+        next = slidePointer[slideclass] - 1;
+        if (next < 0) next += $(selector).length;
+        anim = '+=' + width;
+        width = -width;
+    } else if (direction == 'toleft') {
+        next = slidePointer[slideclass] + 1;
+        if (next > $(selector).length - 1) next -= $(selector).length;
+        anim = '-=' + width;
+    } else {
+        next = direction;
+        anim = '-=' + width;
+    }
+    slidePointer[slideclass] = next;
+    $(selector + '.active').addClass('eliminate');
+    $(selector).eq(next).css('left', width + 'px').addClass('active');
+    $(selector + '.active').animate({left: anim}, 1000, function() {
+        $(selector + '.eliminate').removeClass('active').removeClass('eliminate');
+        $('.slider_points span').removeClass('active').eq(next).addClass('active');
+        slideFlag[slideclass] = false;
+    });
+}
+
+
+function galSlide(direction) {
+    if (galFlag) return;
+    galFlag = true;
+    let hlpstr = parseInt($('.rail').css('left'));
+    if (direction == 'left') {
+        hlpstr -= 60;
+    } else {
+        hlpstr += 60;
+    }
+    $('.rail').animate({
+        left: hlpstr
+    }, 500, function(){
+        let l = parseInt($('.rail').css('left'));
+        if (l == 0) {
+            $('.gal_right').addClass('disabled');
+        } else {
+            $('.gal_right').removeClass('disabled');
+        }
+        if ($('.rail').width() + l == 410) {
+            $('.gal_left').addClass('disabled');
+        } else {
+            $('.gal_left').removeClass('disabled');
+        }
+        galFlag = false;
+    });
+}
+
+
+function getCurrency1() {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://www.cbr-xml-daily.ru/daily_json.js');
+    xhr.onreadystatechange = function() {
+        if ((xhr.readyState == 4) && (xhr.status == 200)) {
+            $('#currency1').html(JSON.parse(xhr.response).Valute.USD.Value.toFixed(2) + ' рублей за доллар');
+        }
+    };
+    xhr.send();
+}
+
+function getCurrency2() {
+    $.get('https://www.cbr-xml-daily.ru/daily_json.js', function(response){
+        $('#currency2').html(JSON.parse(response).Valute.EUR.Value.toFixed(2) + ' рублей за евро');
+    });
+}
+
+function formValidate(form) {
+    let name = $('#name').val();
+    if (!name) {
+        alert('Не заполнено имя!');
+        return false;
+    }
+    let phone = $('#phone').val();
+    if (!phone.match(/^((\+7)|(8))?\s?\(?\d{3}\)?\s?\d{3}\-?\d{2}\-?\d{2}$/)) {
+        alert('Не заполнен номер телефона!');
+        return false;
+    }
+    let mail = $('#mail').val();
+    if (!mail.match(/^.+@.+\..+$/)) {
+        alert('Не заполнен адрес почты!');
+        return false;
+    }
+    let date = $('#date').val();
+    if (!date.match(/^\d{2}\-\d{2}\-\d{4}$/)) {
+        alert('Не выбрана дата!');
+        return false;
+    }
+    let comment = $('#comment').val();
+    let formData = {
+        name,
+        phone,
+        mail,
+        date,
+        comment
+    }
+    $.ajax({
+        url: 'https://jsonplaceholder.typicode.com/posts',
+        data: formData,
+        method: 'POST',
+        success: function(response) {
+            makeAlert(response);
+        }
+    });
+}
+
+function makeAlert(response) {
+    let hlpstr = '<div class="alertbox"><button type="button">&times;</button><p>Ваш заказ оформлен под номером ' + response.id + '.</p></div>';
+    $('body').append('<div class="screen"></div>');
+    $('body').append(hlpstr);
+    $('.alertbox button, .screen').click(function(){
+        $('.alertbox').animate({opacity:0}, 500, function(){
+            location.reload(true);
+        });
+    });
+    $('.alertbox').animate({opacity:1}, 500);
+}
+
+
+
